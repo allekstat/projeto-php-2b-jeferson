f921866d560e3f34c16ab91f97a6d2be5489d700
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-05-19 22:23:20 -0300

    arquivo api

---
+++ api.php
@@ -0,0 +1,22 @@
+<?php include 'backend.php';
+if ($_SERVER['REQUEST_METHOD'] === 'GET')
+{
+    if ($_REQUEST['tabela'] === 'materiais')
+    {
+        $dados = select('*', 'materiais');
+        retorno($dados);
+    }
+}
+else if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    if (isset($_REQUEST['deletar']))
+    {
+        $id = delete('materiais', $_REQUEST['condicoes']);
+        retorno(['status' => 'ok', 'id' => $id]);
+    }
+    else if ($_REQUEST['tabela'] === 'materiais')
+    {
+        $id = insert('materiais', $_REQUEST['campos'], $_REQUEST['valores']);
+        retorno(['status' => 'ok', 'id' => $id]);
+    }
+}
